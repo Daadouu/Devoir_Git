dbb547c95e45eed45e692adcc01f07efeee19237
Adrien Dauvilaire <daadouu--@hotmail.fr>
2018-01-05 11:40:31 +0100

    Addition gauche droite

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 public class HelloWorld {
+	public static void addition (long gauche, long droite)
+	{
+		gauche = gauche + droite;
+	}
+	
        public static void main (String [] args) {
               System.out.println("Hello World");
      }
