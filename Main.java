4c71120d2026a2180be277bbf0b85dae5615470a
Adrien Dauvilaire <daadouu--@hotmail.fr>
2018-01-05 12:18:05 +0100

    2.3.1 Ecrivez la classe décrite ci-dessus

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ public class HelloWorld {
               System.out.println("addition de la case 0");
               System.out.println("creation de deux tableau de long d'une case");
               System.out.println("2.2.3 passer ces deux variable à la fonction d'addition de tableau");
-              System.out.println("modification variables en arguments");
+              System.out.println("2.3.1 Ecrivez la classe décriteci-dessus");
               System.out.println("modification variables en arguments");
               System.out.println("modification variables en arguments");
               System.out.println("modification variables en arguments");
