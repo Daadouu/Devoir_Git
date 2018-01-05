28dbdec0bfe8ee8edcfe79f9d810f20baf101fed
Adrien Dauvilaire <daadouu--@hotmail.fr>
2018-01-05 11:51:08 +0100

    Initialisation des variables

--- Main.java
+++ Main.java
@@ -5,6 +5,14 @@ public class HelloWorld {
 	}
 	
        public static void main (String [] args) {
+	      long g = 2;
+	      long d = 3;
+	      System.out.println("g = " );
+	      system.out.println (g);
+	      System.out.println("d = " );
+	      system.out.println (d);
+
+
               System.out.println("Hello World");
      }
 }
