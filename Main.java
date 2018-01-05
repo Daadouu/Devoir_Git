e2192df4d014a58da4a0cc033ea0d61791ad6617
Adrien Dauvilaire <daadouu--@hotmail.fr>
2018-01-05 12:23:04 +0100

    2.3.3

--- Main.java
+++ Main.java
@@ -20,7 +20,7 @@ public class HelloWorld {
               System.out.println("2.2.3 passer ces deux variable à la fonction d'addition de tableau");
               System.out.println("2.3.1 Ecrivez la classe décriteci-dessus");
               System.out.println("2.3.2 Ecrivez dans la classe Coordonnees");
-              System.out.println("modification variables en arguments");
+              System.out.println("2.3.3 creer une methode statique qui prendra deux arguement de type coordonnées");
               System.out.println("modification variables en arguments");
               System.out.println("modification variables en arguments");
 
